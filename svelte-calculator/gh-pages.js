23b024ff0aa9c6322c189d766bf15344e165b987
wynclefe <wynclefe@gmail.com>
2023-04-18 12:07:41 +0800

    initial gh-pages commit

---
+++ svelte-calculator/gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/Wynclefe/svelte-calculator', // Update to point to your repository  
+        user: {
+            name: 'Wynclef Enerio', // update to use your name
+            email: 'wynclefe@gmail.com' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
