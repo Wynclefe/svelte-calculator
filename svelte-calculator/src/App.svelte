daf93715957cd45423ccaabac4cd89bb34b232f8
wynclefe <wynclefe@gmail.com>
2023-04-18 11:10:22 +0800

    Add display div and buttons

--- svelte-calculator/src/App.svelte
+++ svelte-calculator/src/App.svelte
@@ -27,5 +27,25 @@
 
 <div class="container">
 	<div class="calculator">
+		<div class="display">{displayValue}</div>
+		<div class="buttons">
+			<button on:click={handleButtonCLick}>1</button>
+			<button on:click={handleButtonCLick}>2</button>
+			<button on:click={handleButtonCLick}>3</button>
+			<button on:click={handleButtonCLick}>+</button>
+			<button on:click={handleButtonCLick}>4</button>
+			<button on:click={handleButtonCLick}>5</button>
+			<button on:click={handleButtonCLick}>6</button>
+			<button on:click={handleButtonCLick}>-</button>
+			<button on:click={handleButtonCLick}>7</button>
+			<button on:click={handleButtonCLick}>8</button>
+			<button on:click={handleButtonCLick}>9</button>
+			<button on:click={handleButtonCLick}>*</button>
+			<button on:click={handleButtonCLick}>0</button>
+			<button on:click={handleButtonCLick}>.</button>
+			<button on:click={clearDisplay}>C</button>
+			<button on:click={handleButtonCLick}>/</button>
+			<button on:click={calculateResult}>=</button>
+		</div>
 	</div>
 </div>
