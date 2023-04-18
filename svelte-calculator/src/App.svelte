56a2479f868470ec5f84c727c2cc91115734ca8c
wynclefe <wynclefe@gmail.com>
2023-04-18 10:46:45 +0800

    Add handleButtonClick function for number buttons

--- svelte-calculator/src/App.svelte
+++ svelte-calculator/src/App.svelte
@@ -2,5 +2,10 @@
 	let displayValue = "";
 	let currentValue = false;
 
-	
+	function handleButtonCLick(event) {
+		const buttonValue = event.target.textContent;
+		displayValue += buttonValue;
+		currentValue;
+	}
+
 </script>
